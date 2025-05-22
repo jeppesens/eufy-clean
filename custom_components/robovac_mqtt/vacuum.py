a37ac855ba1cd2a94a62eb452daf6b9fa737d3c9
CBDesignS <54717994+CBDesignS@users.noreply.github.com>
2025-05-22 22:12:05 +0100

    Update vacuum.py with Battery Charge Level sensor
    add sensor for battery charge level so it can be used on the main Entities Card beside the current vac status so you do not have to click the entitie to find out the charge level now that off peak charging has been added to the main app.

--- custom_components/robovac_mqtt/vacuum.py
+++ custom_components/robovac_mqtt/vacuum.py
@@ -1,31 +1,25 @@
 import logging
 from typing import Literal
 
-from homeassistant.components.vacuum import (StateVacuumEntity,
-                                             VacuumEntityFeature)
-from homeassistant.components.vacuum.const import VacuumActivity
+from homeassistant.components.vacuum import StateVacuumEntity, VacuumEntityFeature
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import Entity, DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .constants.hass import DOMAIN, VACS
-from .constants.state import (EUFY_CLEAN_CLEAN_SPEED,
-                              EUFY_CLEAN_NOVEL_CLEAN_SPEED)
+from .constants.state import EUFY_CLEAN_CLEAN_SPEED, EUFY_CLEAN_NOVEL_CLEAN_SPEED
 from .controllers.MqttConnect import MqttConnect
 from .EufyClean import EufyClean
 
 _LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
-    """Initialize my test integration 2 config entry."""
-
     username = config_entry.data[CONF_USERNAME]
     password = config_entry.data[CONF_PASSWORD]
 
@@ -39,45 +33,54 @@ async def async_setup_entry(
         entity = RoboVacMQTTEntity(device)
         hass.data[DOMAIN][VACS][device.device_id] = entity
         async_add_entities([entity])
+
+        battery_sensor = RobovacBatterySensor(device)
+        async_add_entities([battery_sensor])
+
         await entity.pushed_update_handler()
 
+class RobovacBatterySensor(Entity):
+    def __init__(self, robovac):
+        self.robovac = robovac
+        self._attr_unique_id = f"{robovac.device_id}_battery"
+        self._attr_name = f"{robovac.device_model_desc} Battery Level"
+        self._state = None
 
-class RoboVacMQTTEntity(StateVacuumEntity):
-    """Representation of a vacuum cleaner."""
-
-    def __init__(self, item: MqttConnect) -> None:
-        """Initialize Eufy Robovac"""
-        super().__init__()
-        self._battery_level = 0
-        self._state: VacuumActivity = None
-        self._attr_unique_id = item.device_id
-        self._attr_name = item.device_model_desc
-        self._attr_model_code = item.device_model
-
-        self.vacuum = item
-
-        self._attr_fan_speed_list = EUFY_CLEAN_NOVEL_CLEAN_SPEED
-
-        self._attr_mode = None
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, item.device_id)},
-            name=self._attr_name,
-            manufacturer="Eufy",
-            model=self._attr_model_code,
-        )
-
-        self.error_code = None
-
-        item.add_listener(self.pushed_update_handler)
+    async def async_update(self):
+        if hasattr(self.robovac, "get_battery_level"):
+            self._state = await self.robovac.get_battery_level()
 
     @property
-    def activity(self) -> VacuumActivity:
+    def state(self):
         return self._state
 
     @property
-    def supported_features(self) -> VacuumEntityFeature:
-        """Flag supported features."""
-        supported_features = (
+    def unit_of_measurement(self):
+        return "%"
+
+    @property
+    def device_class(self):
+        return "battery"
+
+class RoboVacMQTTEntity(StateVacuumEntity):
+    def __init__(self, item: MqttConnect) -> None:
+        super().__init__()
+        self.vacuum = item
+        self._attr_unique_id = item.device_id
+        self._attr_name = item.device_model_desc
+        self._attr_model = item.device_model
+        self._attr_available = True
+        self._attr_fan_speed_list = EUFY_CLEAN_NOVEL_CLEAN_SPEED
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, item.device_id)},
+            name=item.device_model_desc,
+            manufacturer="Eufy",
+            model=item.device_model,
+        )
+        self._state = None
+        self._attr_battery_level = None
+        self._attr_fan_speed = None
+        self._attr_supported_features = (
             VacuumEntityFeature.START
             | VacuumEntityFeature.PAUSE
             | VacuumEntityFeature.STOP
@@ -88,22 +91,28 @@ class RoboVacMQTTEntity(StateVacuumEntity):
             | VacuumEntityFeature.RETURN_HOME
             | VacuumEntityFeature.SEND_COMMAND
         )
+        item.add_listener(self.pushed_update_handler)
 
-        return supported_features
+    @property
+    def state(self):
+        return self._state or "idle"
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "battery_level": self._attr_battery_level,
+        }
 
     async def pushed_update_handler(self):
         await self.update_entity_values()
-        self.schedule_update_ha_state()
+        self.async_write_ha_state()
 
     async def update_entity_values(self):
         self._attr_battery_level = await self.vacuum.get_battery_level()
         self._state = await self.vacuum.get_work_status()
-        self._attr_mode = await self.vacuum.get_work_mode()
         self._attr_fan_speed = await self.vacuum.get_clean_speed()
 
     async def async_return_to_base(self, **kwargs):
-        """Set the vacuum cleaner to return to the dock."""
-        _LOGGER.info("Return home Pressed")
         await self.vacuum.go_home()
 
     async def async_start(self, **kwargs):
@@ -116,22 +125,19 @@ class RoboVacMQTTEntity(StateVacuumEntity):
         await self.vacuum.stop()
 
     async def async_clean_spot(self, **kwargs):
-        """Perform a spot clean-up."""
-        _LOGGER.info("Spot Clean Pressed")
         await self.vacuum.spot_clean()
 
     async def async_set_fan_speed(self, fan_speed: str, **kwargs):
-        """Set fan speed."""
         if fan_speed not in EUFY_CLEAN_CLEAN_SPEED:
             raise ValueError(f"Invalid fan speed: {fan_speed}")
-        _LOGGER.info(f"Fan Speed {fan_speed} Selected")
         await self.vacuum.set_clean_speed(fan_speed)
 
     async def async_send_command(
-        self, command: Literal['scene_clean'] | Literal['room_clean'], params: dict | list | None = None, **kwargs
+        self,
+        command: Literal['scene_clean', 'room_clean'],
+        params: dict | list | None = None,
+        **kwargs,
     ) -> None:
-        """Send a command to a vacuum cleaner."""
-        _LOGGER.info("Send Command %s Pressed", command)
         if command == "scene_clean":
             if not params or not isinstance(params, dict) or "scene" not in params:
                 raise ValueError("params[scene] is required for scene_clean command")
@@ -141,10 +147,7 @@ class RoboVacMQTTEntity(StateVacuumEntity):
             if not params or not isinstance(params, dict) or not isinstance(params.get("rooms"), list):
                 raise ValueError("params[rooms] is required for room_clean command")
             rooms = [int(r) for r in params['rooms']]
-            if mid := params.get("map_id"):
-                map_id = int(mid)
-                await self.vacuum.room_clean(rooms, map_id)
-            else:
-                await self.vacuum.room_clean(rooms)
+            map_id = int(params.get("map_id", 0))
+            await self.vacuum.room_clean(rooms, map_id)
         else:
-            raise NotImplementedError()
+            raise NotImplementedError(f"Command {command} not implemented")
