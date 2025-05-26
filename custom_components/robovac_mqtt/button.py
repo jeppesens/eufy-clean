31e466d0c7e801527a896c80e8c5966a5e26f93b
Mitchell <mitch@ofzo.info>
2025-05-26 13:31:15 +0200

    Feature: base actions (#29)
    * initial test
    
    * fix init
    
    * working buttons for base actions

---
+++ custom_components/robovac_mqtt/button.py
@@ -0,0 +1,37 @@
+import logging
+
+from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.entity import DeviceInfo
+from .constants.hass import DOMAIN, DEVICES
+from .EufyClean import EufyClean
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    for device_id, device in hass.data[DOMAIN][DEVICES].items():
+        _LOGGER.info("Adding buttons for %s", device_id)
+
+        # Dry mop button
+        dry_mop_button = RoboVacButton(device, "Dry mop", "_dry_mop", device.go_dry)
+        # Clean mop button
+        clean_mop_button = RoboVacButton(device, "Wash mop", "_wash_mop", device.go_selfcleaning)
+        # Empty dust bin button
+        collect_dust_button = RoboVacButton(device, "Empty dust bin", "_empty_dust_bin", device.collect_dust)
+        async_add_entities([dry_mop_button, clean_mop_button, collect_dust_button])
+
+
+class RoboVacButton(ButtonEntity):
+    def __init__(self, device, name, unique_suffix, action):
+        self.vacuum = device
+        self._attr_name = name
+        self._attr_unique_id = device.device_id + unique_suffix
+        self._action = action
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, device.device_id)},
+            name=device.device_model_desc,
+            manufacturer="Eufy",
+            model=device.device_model,
+        )
+
+    async def async_press(self):
+        await self._action()
